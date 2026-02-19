63b4a4983d45c5e0bfb7bafd090b2984655db0bf
chaj <chaithetea@proton.me>
2026-02-19 16:17:05 +0100

    add some networking tools

--- home/home.nix
+++ home/home.nix
@@ -37,13 +37,12 @@
     bat
 
     # # networking tools
-    # iperf3
-    # dnsutils  # `dig` + `nslookup`
-    # ldns # replacement of `dig`, it provide the command `drill`
-    # aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    # socat # replacement of openbsd-netcat
-    # nmap # A utility for network discovery and security auditing
-    # ipcalc  # it is a calculator for the IPv4/v6 addresses
+    mtr
+    gping
+    dnsutils
+    ldns
+    doggo
+    nmap
 
     # misc
     tree
--- hosts/loudness/packages.nix
+++ hosts/loudness/packages.nix
@@ -13,7 +13,6 @@
   # kvm module conflics with vbox, workaround by https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220
   boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
   virtualisation.virtualbox.host.enable = true;
-  # virtualisation.virtualbox.guest.enable = true;
 
   # TODO,
   environment.systemPackages = with pkgs; [
