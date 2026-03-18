a8a20b6859da134887f1bff791b24ae1083770b9
chaj <chaithetea@proton.me>
2026-03-18 22:06:56 +0100

    feat: add some useful stuff iunno

--- modules/home/zed/extensions.nix
+++ modules/home/zed/extensions.nix
@@ -10,7 +10,11 @@
     "elixir"
 
     "uiua" #the color highlighting doesn't separate glyphs on aridity and type which is disappointing
-
+    
+    #web frameworks
+    "astro"
+    "django"
+    
     #build tools
     "dockerfile"
     "docker-compose"
